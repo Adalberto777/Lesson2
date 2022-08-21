89d4e19324980733a6055a556e38da5a4588a5ec
Adalberto777 <albert.tliashev@gmail.com>
2022-08-21 20:19:31 +0500

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// задается число, программа ищет его индекс в массиве, если оно та есть
+int[] array = {1,2,9,4,9,6,7,8,9};
+
+int n = array.Length;
+int find = 9;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //обрывает цикл на первом найденном значении
+    }
+    index++;
+}
