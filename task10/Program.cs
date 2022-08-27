1c7d86261c6511e978334c557e173b284b3c00d3
Adalberto777 <albert.tliashev@gmail.com>
2022-08-27 18:07:17 +0500

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("получаем на вход число и проверяем кратно ли оно одновременно 7 и 23");
+
+Console.WriteLine("Enter a integer number");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 7 ==0 && number % 23 ==0) Console.WriteLine($"{number} кратно 7 и 23");
+
+else Console.WriteLine($"{number} не кратно одновременно 7 и 23");
