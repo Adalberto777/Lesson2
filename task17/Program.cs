2984e59c2ecb4b71c9c43149ac4ef1f2d0784497
Adalberto777 <albert.tliashev@gmail.com>
2022-08-28 19:59:12 +0500

    task17

---
+++ task17/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("получаем на вход число N и выводит квадраты чисел от 1 до N");
+
+Console.WriteLine("Enter a integer number");
+
+int N = int.Parse(Console.ReadLine());
+
+Console.Write(String.Join(" ", Enumerable.Range(1,N).Select(N=>N*N)));
