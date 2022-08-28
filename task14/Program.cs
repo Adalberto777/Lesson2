396429aad4eb301e3dea5977228108d454b08604
Adalberto777 <albert.tliashev@gmail.com>
2022-08-28 18:20:41 +0500

    task14

---
+++ task14/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("получаем на вход поскость в которой находится точка, выводим диапазон координат в которым могут быть равны х и у");
+
+Console.WriteLine("Enter a number from 1 to 4");
+int x = Convert.ToInt32(Console.ReadLine());
+
+//if(x == 1) Console.WriteLine("x от +1 до +бесконечности, у от +1 до +бесконечности ");
+//if(x == 2) Console.WriteLine("x от -1 до -бесконечности, у от +1 до +бесконечности ");
+//if(x == 3) Console.WriteLine("x от -1 до -бесконечности, у от 1 до -бесконечности ");
+//if(x == 4) Console.WriteLine("x от +1 до +бесконечности, у от -1 до -бесконечности ");
+
+switch (x)
+{
+  case 1:
+  {
+      Console.WriteLine("x от +1 до +бесконечности, у от +1 до +бесконечности ");
+      break;
+  }
+   case 2:
+  {
+      Console.WriteLine("x от -1 до -бесконечности, у от +1 до +бесконечности ");
+      break;
+  }
+  case 3:
+  {
+      Console.WriteLine("x от -1 до -бесконечности, у от 1 до -бесконечности ");
+      break;
+  }
+  case 4:
+  {
+      Console.WriteLine("x от +1 до +бесконечности, у от -1 до -бесконечности ");
+      break;
+  }
+}
