d8819db0aef0ccb247e35dd7a926f7fdd5a8a837
Adalberto777 <albert.tliashev@gmail.com>
2022-08-28 20:15:43 +0500

    task18

---
+++ task18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("получаем на вход число N и выводит квадраты чисел от 1 до N");
+
+Console.WriteLine("Enter a integer number");
+
+int N = int.Parse(Console.ReadLine());
+String result = "";
+
+for(int i = 1; i < N + 1; i++)
+{
+    double res = Math.Pow(i,2);
+    result = result + res + " / ";
+}
+
+Console.WriteLine(result);
