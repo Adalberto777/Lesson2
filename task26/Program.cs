dde340cf1955b497d79edc38d5e28a6b70c97c7b
Adalberto777 <albert.tliashev@gmail.com>
2022-08-29 22:39:39 +0500

    task26

---
+++ task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дается число, нужно посчитать количество цифр в числе
+
+int Metod(int a) 
+{
+    int count = 0;
+    int dig = a;
+    
+
+
+    while(dig > 0)
+    {
+        dig = dig / 10;
+        count = count + 1;
+    }
+    return count;
+}
+
+Console.WriteLine("Enter a ");
+int a = int.Parse(Console.ReadLine());
+
+int res = Metod(a);
+Console.WriteLine(res);
