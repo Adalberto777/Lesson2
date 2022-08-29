c674e15ee31e78ae17b23d3890db2932ac199d0b
Adalberto777 <albert.tliashev@gmail.com>
2022-08-29 22:30:19 +0500

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Дается число, необходимо выдать сумму всех чисел от 1 до a
+
+int Metod(int a) 
+{
+    int i = 0;
+    int sum = 0;
+
+    while(i < a)
+    {
+        sum = sum + i;
+        i++;
+    }
+    return sum;
+}
+
+Console.WriteLine("Enter a ");
+int a = int.Parse(Console.ReadLine());
+
+int res = Metod(a);
+Console.WriteLine(res);
+ 
