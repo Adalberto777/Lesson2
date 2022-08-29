550282268705b5b7090ce5a631ebfa3141bec7f9
Adalberto777 <albert.tliashev@gmail.com>
2022-08-30 00:15:17 +0500

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+
+void WriteMassiv(int[] massive) //метод который заполняет массив
+{
+    int lenght = massive.Length;
+    int index = 0;
+    while(index < lenght)
+    {
+        
+        massive[index] = new Random().Next(0,2); // наполняем массив случайными цифрами от 0 до 1 (2-1 так работает Next)
+        index++;
+    }
+}
+
+void PrintMassiv(int[] massiv) // метод который выводит массив
+{
+    int lenght = massiv.Length;
+    int index = 0;
+    while(index < lenght)
+    {
+        Console.Write($"{massiv[index]}|");
+        index++;        
+    }
+}
+
+int[] array = new int[8]; //объявляем массив на 8 элементов
+
+WriteMassiv(array);
+
+Console.Write("|");
+
+PrintMassiv(array);
