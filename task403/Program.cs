a13b5a6a08b1aa611a1aef29690c2af25f2f2408
Adalberto777 <albert.tliashev@gmail.com>
2022-09-03 16:35:47 +0500

    task403

---
+++ task403/Program.cs
@@ -0,0 +1,11 @@
+﻿// Метод вычисления факториала
+int FactorialMenod(int number)
+{
+    if(number == 1 || number ==0) return 1; // 1!=1 0!=1
+    else return number * FactorialMenod(number - 1);
+}
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{number}! = {FactorialMenod(number)}");
