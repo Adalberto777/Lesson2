0ed70864f51abbf033a39d8e91210fcb55583c3f
Adalberto777 <albert.tliashev@gmail.com>
2022-09-07 23:24:13 +0500

    task406

---
+++ task406/Program.cs
@@ -0,0 +1,37 @@
+﻿// программа, которая будет преобразовывать десятичное число в двоичное
+Console.WriteLine("Enter a decimal number");
+// int decimaNumber = int.Parse(Console.ReadLine());
+
+// while(decimaNumber > 0)
+// {
+//     int temp = decimaNumber / 2;
+//     int binaryNumber = decimaNumber - 2 * temp;
+//     Console.Write(binaryNumber);
+//     decimaNumber = temp;
+
+// }
+
+int number = int.Parse(Console.ReadLine());
+
+string res1 = Convert.ToString(number, 2);
+string res2 = DecToNum(number, 2);
+
+Console.WriteLine($"{number}->{res1}");
+
+
+
+
+// Универсальный математический для перевода из 10 в любую
+string DecToNum(int decNumber, int otherSystem)
+{
+string res = "";
+string nums = "0123456789ABCDEF";
+while (decNumber > 0)
+{
+int ost = decNumber / otherSystem;
+res = nums[decNumber - otherSystem * ost] + res;
+decNumber /= otherSystem;
+}
+return res;
+}
+
