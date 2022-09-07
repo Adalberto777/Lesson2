59edf38d19b1596a0c20942eb966ec0654b7f6af
Adalberto777 <albert.tliashev@gmail.com>
2022-09-07 23:33:50 +0500

    task407

---
+++ task407/Program.cs
@@ -0,0 +1,25 @@
+﻿// программа, которая будет преобразовывать десятичное число в требуемое
+Console.WriteLine("Enter a decimal number");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a system number");
+int system = int.Parse(Console.ReadLine());
+
+string res = DecToNum(number, system);
+
+Console.WriteLine($"{number}->{res}");
+
+string DecToNum(int decNumber, int otherSystem)
+{
+string result = "";
+string nums = "0123456789ABCDEF";
+while (decNumber > 0)
+{
+int ost = decNumber / otherSystem;
+result = nums[decNumber - otherSystem * ost] + result;
+decNumber /= otherSystem;
+}
+return result;
+}
+
+
