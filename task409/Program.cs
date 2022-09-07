e0af7034210cd67fd3b8524d0c5b1c4034f25e7e
Adalberto777 <albert.tliashev@gmail.com>
2022-09-08 00:24:55 +0500

    task409

---
+++ task409/Program.cs
@@ -0,0 +1,8 @@
+﻿// программа, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины
+Console.WriteLine("введите три числа через пробел");
+string[] st = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries); //считывает строку, когда видит пробел 
+                                                                                    //отделяет выражение и записывает в массив строк
+bool IsTriangle(int a, int b, int c) 
+{ return (((a + b) > c) && ((a + c) > b) && ((b + c) > a)); }
+
+Console.WriteLine(IsTriangle(int.Parse(st[0]), int.Parse(st[1]), int.Parse(st[2])));
