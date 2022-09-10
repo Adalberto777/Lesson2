249271441d5ad28c913c7c790e0c4c8c863c3ac0
Adalberto777 <albert.tliashev@gmail.com>
2022-09-10 23:08:31 +0500

    410

--- task409/Program.cs
+++ task409/Program.cs
@@ -1,6 +1,6 @@
 ﻿// программа, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины
 Console.WriteLine("введите три числа через пробел");
-string[] st = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries); //считывает строку, когда видит пробел 
+string[] st = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries); //считывает строку, когда видит пробел  
                                                                                     //отделяет выражение и записывает в массив строк
 bool IsTriangle(int a, int b, int c) 
 { return (((a + b) > c) && ((a + c) > b) && ((b + c) > a)); }
---
+++ task410/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задаем двумерный массив из случайных чисел размером m×n
+Console.WriteLine("введите количество строк");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите количество столбцов");
+int m = int.Parse(Console.ReadLine());
+
+int[,] GetMatrix(int lines, int columns, int minValue, int maxValue) //функция GetArray с параметрами: размер, мин элемент, макс элемент создает массив 
+{                                                    //заполненный случайными числами
+  int[,] result = new int[lines, columns];
+
+  for (int i = 0; i < result.GetLength(0); i++)
+  {
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+        result[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+  return result;
+}
+
+
+void PrintMatrix (int[,] matrix1)
+{
+    for (int i = 0; i < matrix1.GetLength(0); i++)  // matrix.GetLength(0) конструкция выдает количество строк или столбцов 
+    {                                              // в зависимости 0 или 1 (0- строки, 1 столбцы)
+        for (int j = 0; j < matrix1.GetLength(1); j++)
+    {
+        Console.Write($"{matrix1[i, j]} ");
+    }
+    Console.WriteLine();
+    }
+}
+
+int[,] matrix = GetMatrix(n, m, 1, 9); //1-строки 2-столбцы 3-минимальное число 4-максимальное число
+PrintMatrix(matrix);
+
+  
