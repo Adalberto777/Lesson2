cf0e304f9c3d4acdea67cc8bea51eb4594cd9376
Adalberto777 <albert.tliashev@gmail.com>
2022-09-19 20:25:27 +0500

    task500

---
+++ task500/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N
+Console.WriteLine("Введите число N");
+int n2 = int.Parse(Console.ReadLine());
+int n1 = 1;
+
+string PrintNumbers(int start, int end)
+{
+if (start == end) return start.ToString();
+return (start + " " + PrintNumbers(start + 1, end));
+}
+
+ Console.Write(PrintNumbers(n1, n2));
+
